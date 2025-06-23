36756bafe2d2d3ba5c787011deef0fcf309be6dc
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-06-24 00:26:57 +0600

    1807A - Plus or Minus solve

--- 1807A - Plus or Minus.c
+++ 1807A - Plus or Minus.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-    if(a + b == c){
-        printf("+\n");
-    } else{
-        printf("-\n");
+    int t;
+    scanf("%d", &t);
+    
+    while(t--) {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+        if (a + b == c){
+            printf("+\n");
+        } else{
+            printf("-\n");
+        }
     }
+
     return 0;
 }

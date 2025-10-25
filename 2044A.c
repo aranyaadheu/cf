bf33d1298f9b470e44ba52f0c655a1c5fbee6d90
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-10-25 13:01:31 +0600

    Create 2044A.c

---
+++ 2044A.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        int n, a, b = 1;
+        scanf("%d", &n);
+        a = n - b;
+        printf("%d\n", a);
+
+    }
+
+    return 0;
+}

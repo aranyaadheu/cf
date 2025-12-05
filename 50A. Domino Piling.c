b67a9422f58c5961c5072e682db2a088f67e8ec4
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-12-05 17:08:54 +0600

    Update 50A. Domino Piling.c

---
+++ 50A. Domino Piling.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main(){
+    int m, n, s;
+    scanf("%d %d", &m, &n);
+    
+    s = m * n;
+    
+    if(s%2==0){
+        printf("%d", s / 2);
+    } 
+    else{
+        printf("%d", (s-1) / 2);
+    }
+    
+    
+    return 0;
+}

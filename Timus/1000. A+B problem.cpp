6cabbece0a34a53544928a6da13c81b6cfe286c4
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-12-11 13:55:51 +0600

    TimusOJ problems

---
+++ Timus/1000. A+B problem.cpp
@@ -0,0 +1,9 @@
+// Timus 1000. A+b problem
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a + b);
+    return 0;
+}
---
+++ Timus/1293.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// input 5 2 3, output 60
+int main(){
+    int n, a, b;
+    scanf("%d %d %d", &n, &a, &b);
+    printf("%d", 2*(n*a*b));
+    return 0;
+}

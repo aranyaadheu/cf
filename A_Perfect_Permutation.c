1289f6ef7a145a33413e89396707e7dc71b6455d
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-12-24 16:37:40 +0600

    Create A_Perfect_Permutation.c

---
+++ A_Perfect_Permutation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    
+    if(n % 2 != 0)
+    {
+        printf("-1\n");
+        return 0;
+    }
+    
+    for(int i = 1; i <= n; i += 2)
+    {
+        printf("%d %d ", i + 1, i);
+    }
+    
+    return 0;
+}
